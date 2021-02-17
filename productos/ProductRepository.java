17dcc85c1dfaac6d2dd986cd00da87cf4784b159
aarabasco <78800050+aarabasco@users.noreply.github.com>
2021-02-17 11:15:39 +0100

    Added functions
    Added getAllDrinks and getAllFoods

--- productos/ProductRepository.java
+++ productos/ProductRepository.java
@@ -20,9 +20,26 @@ public class ProductRepository{
 	public void addProduct(Product product){
 		products.add(product);
 	}
+	public void removeProduct(Product product){
+		products.remove(product);
+	}
 	public void gelAllProducts(){
 		for(int i=0;i<products.size();i++){
 			System.out.println(products.get(i));
+		}
+	}
+	public void gelAllDrinks(){
+		for(int i=0;i<products.size();i++){
+			if(products.get(i) instanceof Drink){
+				System.out.println(products.get(i));
+			}
+		}
+	}
+	public void gelAllFoods(){
+		for(int i=0;i<products.size();i++){
+			if(products.get(i) instanceof Food){
+				System.out.println(products.get(i));
+			}
 		}
 	}
 }
