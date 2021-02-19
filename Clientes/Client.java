0be8342df325b9314c8398a0405c6225e998a6e8
David Sánchez Morales <78800247+DavidSM24@users.noreply.github.com>
2021-02-19 19:43:15 +0100

    Update Client.java

--- Clientes/Client.java
+++ Clientes/Client.java
@@ -1,13 +1,16 @@
 package Clientes;
+
+import java.util.ArrayList;
+
 public class Client extends Person {
 	
 	private String address; 
-	private int orders;
+	private ArrayList<Order> orders;
 	private int points;
 	
 	
 	
-	public Client(String name, int dni, int age, String address, int orders, int points) {
+	public Client(String name, String dni, int age, String address, ArrayList<Order> orders, int points) {
 		super(name, dni, age);
 		this.address = address;
 		this.orders = orders;
@@ -22,11 +25,11 @@ public class Client extends Person {
 		this.address = address;
 	}
 	
-	public int getOrders() {
+	public ArrayList<Order> getOrders() {
 		return orders;
 	}
 
-	public void setOrders(int orders) {
+	public void setOrders(ArrayList<Order> orders) {
 		this.orders = orders;
 	}
 
@@ -37,5 +40,23 @@ public class Client extends Person {
 	public void setPoints(int points) {
 		this.points = points;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Client "+super.toString()+" address=" + address + ", orders=" + orders + ", points=" + points + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean result=super.equals(obj);
+		return result;
+	}
 }
