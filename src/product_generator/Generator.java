ac077a263f0170077e8ade939960d8253fbdde24
David Sanchez <david.4.1.96.dam@gmail.com>
2021-03-12 12:23:27 +0100

    Revert "update product_generator"
    This reverts commit 2295030ace82e49cba0c213871c9f3684ffea7aa.

--- src/product_generator/Generator.java
+++ src/product_generator/Generator.java
@@ -76,11 +76,7 @@ public class Generator {
 			if(option==1) {
 				vegan=true;
 			}
-			Integer[]bundlePackA=new Integer[bundlePack.size()];
-			for (int i = 0; i < bundlePackA.length; i++) {
-				bundlePackA[i]=bundlePack.get(i);
-			}
-			
+			Integer[]bundlePackA=(Integer[]) bundlePack.toArray();
 			result=new Food(name, price, forCeliac, vegan,bundlePackA);
 		}
 		
@@ -93,12 +89,8 @@ public class Generator {
 			if(option==1) {
 				alcoholic=true;
 			}
-
-			Integer[]bundlePackA=new Integer[bundlePack.size()];
-			for (int i = 0; i < bundlePackA.length; i++) {
-				bundlePackA[i]=bundlePack.get(i);
-			}
 			
+			Integer[]bundlePackA=(Integer[]) bundlePack.toArray();
 			result=new Drink(name, price, forCeliac, alcoholic,bundlePackA);
 		}
 		
